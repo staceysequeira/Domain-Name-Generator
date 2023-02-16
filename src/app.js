6233fd7792d11b38e7dd3a61280584928ae0d561
staceysequeira <ssequ002@gmail.com>
2023-02-16 13:20:28 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["crafty", "loud"];
+  let noun = ["platypus", "mime"];
+  let domainNames = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        domainNames.push(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+      }
+    }
+  }
+  function adddomainNames() {
+    let template = domainNames.map(element => `<li>${element}</li>`).join("\n");
+    document.querySelector("ul").innerHTML = template;
+  }
+  adddomainNames();
 };
